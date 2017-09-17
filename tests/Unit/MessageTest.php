6bc00f080f14b47ced9e380dec172c3d48f3d248
John Markese <john.markese@gmail.com>
2017-09-17 14:51:30 -0500

    create Message and Session Endpoint Unit tests

---
+++ tests/Unit/MessageTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class MessageTest extends TestCase
+{
+    /**
+     * Test the Message Index Endpoint.
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Test the Message Create Endpoint.
+     *
+     * @return void
+     */
+    public function testCreate()
+    {
+        $this->assertTrue(true);
+    }
+
+}
---
+++ tests/Unit/SessionTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SessionTest extends TestCase
+{
+    /**
+     * Test the Session Create Endpoint.
+     *
+     * @return void
+     */
+    public function testCreate()
+    {
+        $this->assertTrue(true);
+    }
+}
